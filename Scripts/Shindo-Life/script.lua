80e35d64a1e5fbea8114c60dc60b361f272d3e95
EduPro <47499166+edugamerplay1228@users.noreply.github.com>
2022-04-19 19:30:55 -0300

    Update script.lua

--- Scripts/Shindo-Life/script.lua
+++ Scripts/Shindo-Life/script.lua
@@ -12,12 +12,21 @@ local TextBox = Instance.new("TextBox")
 local Frame_3 = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
 local Frame_4 = Instance.new("Frame")
+local Main = Instance.new("Frame")
+local Frame_5 = Instance.new("Frame")
+local TextLabel_2 = Instance.new("TextLabel")
+local KG1 = Instance.new("TextBox")
+local TextLabel_3 = Instance.new("TextLabel")
+local Frame_6 = Instance.new("Frame")
+local KG2 = Instance.new("TextBox")
+local TextLabel_4 = Instance.new("TextLabel")
+local Frame_7 = Instance.new("Frame")
+local TextLabel_5 = Instance.new("TextLabel")
 
 --Properties:
 
 GetCodeGUI.Name = "GetCodeGUI"
 GetCodeGUI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-GetCodeGUI.Enabled = false
 GetCodeGUI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 CodeArea.Name = "CodeArea"
@@ -88,9 +97,121 @@ Frame_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 Frame_4.Size = UDim2.new(0, 199, 0, 49)
 Frame_4.Style = Enum.FrameStyle.RobloxRound
 
+Main.Name = "Main"
+Main.Parent = GetCodeGUI
+Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Main.Position = UDim2.new(0.371272355, 0, 0.294478536, 0)
+Main.Size = UDim2.new(0, 517, 0, 334)
+Main.Visible = false
+Main.ZIndex = 0
+Main.Style = Enum.FrameStyle.RobloxRound
+
+Frame_5.Parent = Main
+Frame_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_5.Position = UDim2.new(-0.0153921917, 0, -0.0253580995, 0)
+Frame_5.Size = UDim2.new(0, 517, 0, 57)
+Frame_5.Style = Enum.FrameStyle.RobloxRound
+
+TextLabel_2.Parent = Frame_5
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 1.000
+TextLabel_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel_2.BorderSizePixel = 0
+TextLabel_2.Position = UDim2.new(-0.0237688832, 0, -0.154201031, 0)
+TextLabel_2.Size = UDim2.new(0, 523, 0, 55)
+TextLabel_2.ZIndex = 3
+TextLabel_2.Font = Enum.Font.Oswald
+TextLabel_2.Text = "[BETA] Shindo Life GUI"
+TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.TextScaled = true
+TextLabel_2.TextSize = 14.000
+TextLabel_2.TextWrapped = true
+
+KG1.Name = "KG1"
+KG1.Parent = Main
+KG1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+KG1.BackgroundTransparency = 1.000
+KG1.BorderSizePixel = 0
+KG1.Position = UDim2.new(0.572163403, 0, 0.203538999, 0)
+KG1.Size = UDim2.new(0, 200, 0, 39)
+KG1.ZIndex = 3
+KG1.Font = Enum.Font.SourceSans
+KG1.Text = ""
+KG1.TextColor3 = Color3.fromRGB(255, 255, 255)
+KG1.TextSize = 14.000
+
+TextLabel_3.Parent = KG1
+TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.BackgroundTransparency = 1.000
+TextLabel_3.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel_3.BorderSizePixel = 0
+TextLabel_3.Position = UDim2.new(-0.273768932, 0, 0.0765678212, 0)
+TextLabel_3.Size = UDim2.new(0, 54, 0, 33)
+TextLabel_3.ZIndex = 3
+TextLabel_3.Font = Enum.Font.Oswald
+TextLabel_3.Text = "KG1:"
+TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_3.TextScaled = true
+TextLabel_3.TextSize = 14.000
+TextLabel_3.TextWrapped = true
+
+Frame_6.Parent = KG1
+Frame_6.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_6.Position = UDim2.new(0, 0, 0.0765678212, 0)
+Frame_6.Size = UDim2.new(0, 199, 0, 32)
+Frame_6.Style = Enum.FrameStyle.RobloxRound
+
+KG2.Name = "KG2"
+KG2.Parent = Main
+KG2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+KG2.BackgroundTransparency = 1.000
+KG2.BorderSizePixel = 0
+KG2.Position = UDim2.new(0.572163403, 0, 0.349999994, 0)
+KG2.Size = UDim2.new(0, 200, 0, 39)
+KG2.ZIndex = 3
+KG2.Font = Enum.Font.SourceSans
+KG2.Text = ""
+KG2.TextColor3 = Color3.fromRGB(255, 255, 255)
+KG2.TextSize = 14.000
+
+TextLabel_4.Parent = KG2
+TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.BackgroundTransparency = 1.000
+TextLabel_4.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel_4.BorderSizePixel = 0
+TextLabel_4.Position = UDim2.new(-0.273768932, 0, 0.0765678212, 0)
+TextLabel_4.Size = UDim2.new(0, 54, 0, 33)
+TextLabel_4.ZIndex = 3
+TextLabel_4.Font = Enum.Font.Oswald
+TextLabel_4.Text = "KG2:"
+TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_4.TextScaled = true
+TextLabel_4.TextSize = 14.000
+TextLabel_4.TextWrapped = true
+
+Frame_7.Parent = KG2
+Frame_7.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Frame_7.Position = UDim2.new(0, 0, 0.0765678212, 0)
+Frame_7.Size = UDim2.new(0, 199, 0, 32)
+Frame_7.Style = Enum.FrameStyle.RobloxRound
+
+TextLabel_5.Parent = Main
+TextLabel_5.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.BackgroundTransparency = 1.000
+TextLabel_5.BorderColor3 = Color3.fromRGB(27, 42, 53)
+TextLabel_5.BorderSizePixel = 0
+TextLabel_5.Position = UDim2.new(-0.0628101304, 0, 0.924442291, 0)
+TextLabel_5.Size = UDim2.new(0, 239, 0, 31)
+TextLabel_5.ZIndex = 3
+TextLabel_5.Font = Enum.Font.Oswald
+TextLabel_5.Text = "Made by; EduGamerPlay1228"
+TextLabel_5.TextColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_5.TextSize = 24.000
+TextLabel_5.TextWrapped = true
+
 -- Scripts:
 
-local function KQLC_fake_script() -- Frame_2.VerifyScript 
+local function WMZSTMO_fake_script() -- Frame_2.VerifyScript 
 	local script = Instance.new('LocalScript', Frame_2)
 
 	local http = game:GetService("HttpService")
@@ -103,157 +224,10 @@ local function KQLC_fake_script() -- Frame_2.VerifyScript
 	
 	
 	function LoadScript()
-		-- Gui to Lua
-		-- Version: 3.2
-	
-		-- Instances:
-	
-		local Hack = Instance.new("ScreenGui")
-		local Main = Instance.new("Frame")
-		local Frame = Instance.new("Frame")
-		local TextLabel = Instance.new("TextLabel")
-		local KG1 = Instance.new("TextBox")
-		local TextLabel_2 = Instance.new("TextLabel")
-		local Frame_2 = Instance.new("Frame")
-		local KG2 = Instance.new("TextBox")
-		local TextLabel_3 = Instance.new("TextLabel")
-		local Frame_3 = Instance.new("Frame")
-		local TextLabel_4 = Instance.new("TextLabel")
-	
-		--Properties:
-	
-		Hack.Name = "Hack"
-		Hack.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
-	
-		Main.Name = "Main"
-		Main.Parent = Hack
-		Main.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Main.Position = UDim2.new(0.371272355, 0, 0.294478536, 0)
-		Main.Size = UDim2.new(0, 517, 0, 334)
-		Main.ZIndex = 0
-		Main.Style = Enum.FrameStyle.RobloxRound
-	
-		Frame.Parent = Main
-		Frame.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Frame.Position = UDim2.new(-0.0153921917, 0, -0.0253580995, 0)
-		Frame.Size = UDim2.new(0, 517, 0, 57)
-		Frame.Style = Enum.FrameStyle.RobloxRound
-	
-		TextLabel.Parent = Frame
-		TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel.BackgroundTransparency = 1.000
-		TextLabel.BorderColor3 = Color3.fromRGB(27, 42, 53)
-		TextLabel.BorderSizePixel = 0
-		TextLabel.Position = UDim2.new(-0.0237688832, 0, -0.154201031, 0)
-		TextLabel.Size = UDim2.new(0, 523, 0, 55)
-		TextLabel.ZIndex = 3
-		TextLabel.Font = Enum.Font.Oswald
-		TextLabel.Text = "[BETA] Shindo Life GUI"
-		TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel.TextScaled = true
-		TextLabel.TextSize = 14.000
-		TextLabel.TextWrapped = true
-	
-		KG1.Name = "KG1"
-		KG1.Parent = Main
-		KG1.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		KG1.BackgroundTransparency = 1.000
-		KG1.BorderSizePixel = 0
-		KG1.Position = UDim2.new(0.572163403, 0, 0.203538999, 0)
-		KG1.Size = UDim2.new(0, 200, 0, 39)
-		KG1.ZIndex = 3
-		KG1.Font = Enum.Font.SourceSans
-		KG1.Text = ""
-		KG1.TextColor3 = Color3.fromRGB(255, 255, 255)
-		KG1.TextSize = 14.000
-	
-		TextLabel_2.Parent = KG1
-		TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel_2.BackgroundTransparency = 1.000
-		TextLabel_2.BorderColor3 = Color3.fromRGB(27, 42, 53)
-		TextLabel_2.BorderSizePixel = 0
-		TextLabel_2.Position = UDim2.new(-0.273768932, 0, 0.0765678212, 0)
-		TextLabel_2.Size = UDim2.new(0, 54, 0, 33)
-		TextLabel_2.ZIndex = 3
-		TextLabel_2.Font = Enum.Font.Oswald
-		TextLabel_2.Text = "KG1:"
-		TextLabel_2.TextColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel_2.TextScaled = true
-		TextLabel_2.TextSize = 14.000
-		TextLabel_2.TextWrapped = true
-	
-		Frame_2.Parent = KG1
-		Frame_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Frame_2.Position = UDim2.new(0, 0, 0.0765678212, 0)
-		Frame_2.Size = UDim2.new(0, 199, 0, 32)
-		Frame_2.Style = Enum.FrameStyle.RobloxRound
-	
-		KG2.Name = "KG2"
-		KG2.Parent = Main
-		KG2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		KG2.BackgroundTransparency = 1.000
-		KG2.BorderSizePixel = 0
-		KG2.Position = UDim2.new(0.572163403, 0, 0.349999994, 0)
-		KG2.Size = UDim2.new(0, 200, 0, 39)
-		KG2.ZIndex = 3
-		KG2.Font = Enum.Font.SourceSans
-		KG2.Text = ""
-		KG2.TextColor3 = Color3.fromRGB(255, 255, 255)
-		KG2.TextSize = 14.000
-	
-		TextLabel_3.Parent = KG2
-		TextLabel_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel_3.BackgroundTransparency = 1.000
-		TextLabel_3.BorderColor3 = Color3.fromRGB(27, 42, 53)
-		TextLabel_3.BorderSizePixel = 0
-		TextLabel_3.Position = UDim2.new(-0.273768932, 0, 0.0765678212, 0)
-		TextLabel_3.Size = UDim2.new(0, 54, 0, 33)
-		TextLabel_3.ZIndex = 3
-		TextLabel_3.Font = Enum.Font.Oswald
-		TextLabel_3.Text = "KG2:"
-		TextLabel_3.TextColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel_3.TextScaled = true
-		TextLabel_3.TextSize = 14.000
-		TextLabel_3.TextWrapped = true
-	
-		Frame_3.Parent = KG2
-		Frame_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		Frame_3.Position = UDim2.new(0, 0, 0.0765678212, 0)
-		Frame_3.Size = UDim2.new(0, 199, 0, 32)
-		Frame_3.Style = Enum.FrameStyle.RobloxRound
-	
-		TextLabel_4.Parent = Main
-		TextLabel_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel_4.BackgroundTransparency = 1.000
-		TextLabel_4.BorderColor3 = Color3.fromRGB(27, 42, 53)
-		TextLabel_4.BorderSizePixel = 0
-		TextLabel_4.Position = UDim2.new(-0.0628101304, 0, 0.924442291, 0)
-		TextLabel_4.Size = UDim2.new(0, 239, 0, 31)
-		TextLabel_4.ZIndex = 3
-		TextLabel_4.Font = Enum.Font.Oswald
-		TextLabel_4.Text = "Made by; EduGamerPlay1228"
-		TextLabel_4.TextColor3 = Color3.fromRGB(255, 255, 255)
-		TextLabel_4.TextSize = 24.000
-		TextLabel_4.TextWrapped = true
-	
-		-- Scripts:
-	
-		local function WQQSX_fake_script() -- Main.Manager 
-			local script = Instance.new('LocalScript', Main)
-	
-			local KG1 = script.Parent.KG1
-			local KG2 = script.Parent.KG2
-	
-			while wait() do
-				local text = KG1.Text
-				game.Players.LocalPlayer.statz.main.kg1.Value = text
-				local text1 = KG1.Text
-				game.Players.LocalPlayer.statz.main.kg1.Value = text1
-			end
-		end
-		coroutine.wrap(WQQSX_fake_script)()
-	
+		script.Parent.Parent.Parent.Main.Visible = true
 	end
+	
+	
 	
 	Button.MouseButton1Click:Connect(function()
 		if InputArea.Text == realCode then
@@ -276,4 +250,18 @@ local function KQLC_fake_script() -- Frame_2.VerifyScript
 	end)
 	
 end
-coroutine.wrap(KQLC_fake_script)()
+coroutine.wrap(WMZSTMO_fake_script)()
+local function OTDA_fake_script() -- Main.Manager 
+	local script = Instance.new('LocalScript', Main)
+
+	local KG1 = script.Parent.KG1
+	local KG2 = script.Parent.KG2
+	
+	while wait() do
+		local text = KG1.Text
+		game.Players.LocalPlayer.statz.main.kg1.Value = text
+		local text1 = KG2.Text
+		game.Players.LocalPlayer.statz.main.kg1.Value = text1
+	end
+end
+coroutine.wrap(OTDA_fake_script)()
